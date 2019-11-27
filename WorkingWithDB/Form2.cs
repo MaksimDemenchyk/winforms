9cc6d84fdea0d45a104e2d798a9d35275dc3615f
Макс Деменчук <maksimd1996@gmail.com>
2019-11-27 09:24:58 +0300

    Initial

---
+++ WorkingWithDB/Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkingWithDB
+{
+    public partial class Form2 : Form
+    {
+         public Form2()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "database1DataSet.Products". При необходимости она может быть перемещена или удалена.
+            this.productsTableAdapter.Fill(this.database1DataSet.Products);
+            var deleteButton = new DataGridViewButtonColumn();
+            deleteButton.Name = "dataGridViewDeleteButton";
+            deleteButton.HeaderText = " ";
+            deleteButton.Text = "Delete";
+            deleteButton.UseColumnTextForButtonValue = true;
+            this.dataGridView1.Columns.Add(deleteButton);
+            
+            
+
+        }
+        void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+
+            if (e.RowIndex == dataGridView1.NewRowIndex || e.RowIndex < 0)
+                return;
+
+            if (e.ColumnIndex == dataGridView1.Columns["dataGridViewDeleteButton"].Index)
+            {
+                var image = Properties.Resources.dell_button; //An image
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+                var x = e.CellBounds.Left + (e.CellBounds.Width - image.Width) / 2;
+                var y = e.CellBounds.Top + (e.CellBounds.Height - image.Height) / 2;
+                e.Graphics.DrawImage(image, new Point(x, y));
+
+                e.Handled = true;
+            }
+        }
+        
+        void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //if click is on new row or header row
+            if (e.RowIndex == dataGridView1.NewRowIndex || e.RowIndex < 0)
+                return;
+
+            //Check if click is on specific column 
+            if (e.ColumnIndex == dataGridView1.Columns["dataGridViewDeleteButton"].Index)
+            {
+                //Put some logic here, for example to remove row from your binding list.
+                dataGridView1.Rows.RemoveAt(e.RowIndex);
+                dataGridView1.Refresh();
+            }
+        }
+    }
+}
